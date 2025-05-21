57a8bd166c552517c6bbebbe6099539c53a3a1ae
ClaireCompare <claire@markcompare.com>
2025-05-21 14:06:44 +0200

    Set Root and Created View - Set root to Flight#index - Created form for flight search - Add methods to Flight Model to format time and date

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,22 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  def formatted_date
+    start_date.strftime("%d %B %Y")
+  end
+
+  def formatted_time
+    start_time.strftime("%I:%M")
+  end
+
+  def self.formatted_date_options
+    unique_dates = order(:start_date).pluck(:start_date).uniq
+    unique_dates.map { |date| [ date.strftime("%d %B %Y") ] }
+  end
+
+  def self.formatted_time_options
+    unique_times = order(:start_time).pluck(:start_time).uniq
+    unique_times.map { |time| [ time.strftime("%I:%M") ] }
+  end
 end
